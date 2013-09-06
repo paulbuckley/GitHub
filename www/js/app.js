a4fedbc9153700378abcd040ac76f8fed6882541
Paul Buckley <paulbuckley@processfour.com>
2013-09-06 16:27:05 +0100

    test

--- www/js/app.js
+++ www/js/app.js
@@ -64,7 +64,7 @@ var app = function(){
 	               	var html = '';
 					var itemDescription = '<span class="itemDesc">' + data.strDealName + '</span>';
 					// html += '<li id="' + data.strDealName + '"><a href="#"><img src="./img/products/' + index + '.jpg">' + itemDescription + '</a></li>';
-	               	html += '<li id="' + data.strDealName + '"><a href="#"><img src="' + data.strImageListUrl + '">' + itemDescription + '</a></li>';
+	               	html += '<li id="' + data.strDealName + '"><a href="#"><img src="' + data.strImageListUrl + '">' + data.strImageListUrl + ' ' + itemDescription + '</a></li>';
 	               	$('#data_list').append(html); //Append list to container
 	         });
 
